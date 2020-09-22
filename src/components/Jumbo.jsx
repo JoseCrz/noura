788f5b73099c04e94e5dbfcd1cf99a1d120a0fbe
Jose Cuevas <josecueram@gmail.com>
2020-09-21 22:24:32 -0500

    [ADD] Created Jumbo Component

---
+++ src/components/Jumbo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { StyledJumbo } from '../styles/components'
+
+const Jumbo = () => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Noura Gourmet</h2>
+        <small>The best Croissants</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
