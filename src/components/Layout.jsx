a09704c5f2c45299aab5290ad1b7e91f3f76032a
Jose Cuevas <josecueram@gmail.com>
2020-09-24 17:58:53 -0500

    [REMOVE] Unsed Layout Code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,41 +7,22 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
+import { Content, Footer } from '../styles/components'
 
 import Header from './Header'
 import './layout.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title || 'Title'} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0 1.0875rem 1.45rem'
-        }}
-      >
+      <Header />
+      <Content>
         <main>{children}</main>
-        <footer style={{
-          marginTop: '2rem'
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {' '}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+        <Footer>
+          © {new Date().getFullYear()}, Built by
+          <a href="https://github.com/josecrz">José Cuevas</a>
+        </Footer>
+      </Content>
     </>
   )
 }
