a6c6206bf28b1819560228109355dd68c7988757
Jose Cuevas <josecueram@gmail.com>
2020-09-25 17:33:36 -0500

    [REMOVE] Unused elements in Index Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
-import { SEO, Jumbo, Image } from '../components'
+import { SEO, Jumbo } from '../components'
 
 export const query = graphql`
   query GET_METADATA {
@@ -25,12 +25,6 @@ const IndexPage = ({ data }) => {
     <>
       <SEO title="Home" />
       <Jumbo title={title} description={description} />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-        <Image />
-      </div>
       <Link to="/thanks/">Go to thanks page</Link> <br />
       <Link to="/cancel/">Go to cancel page</Link>
     </>
