92f6d79a87c8de4afa4219f24fca7a19410ac686
Jose Cuevas <josecueram@gmail.com>
2020-09-25 13:47:27 -0500

    [ADD] Installed typography gatsby plugin

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Helvetica Neue', 'Arial'],
+  bodyFontFamily: ['Open Sans', 'Roboto']
+})
+
+export default typography
