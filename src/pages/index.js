0e630f775d53cc84e84d0fb48e79eeedd011356c
Jose Cuevas <josecueram@gmail.com>
2020-09-29 20:56:32 -0500

    [ADD] Implemented Products in index Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
-import { SEO, Jumbo } from '../components'
+import { SEO, Jumbo, Products } from '../components'
 
 export const query = graphql`
-  query GET_METADATA {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -13,6 +13,22 @@ export const query = graphql`
             description
             title
           }
+        }
+      }
+    }
+    allStripePrice {
+      edges {
+        node {
+          id
+          product  {
+            name
+            metadata {
+              img
+              description
+              vegan
+            }
+          }
+          unit_amount
         }
       }
     }
@@ -25,6 +41,7 @@ const IndexPage = ({ data }) => {
     <>
       <SEO title="Home" />
       <Jumbo title={title} description={description} />
+      <Products products={data.allStripePrice.edges} />
       <Link to="/thanks/">Go to thanks page</Link> <br />
       <Link to="/cancel/">Go to cancel page</Link>
     </>
