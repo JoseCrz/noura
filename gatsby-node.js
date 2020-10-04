9bad75be8cda3d29922b8ba3d70eb48936d19eba
Jose Cuevas <josecueram@gmail.com>
2020-10-04 15:17:13 -0500

    [ADD] Added context to createPages  API for Template to Consume

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,32 +5,33 @@ exports.createPages = async ({ graphql, actions }) => {
   const productTemplate = path.resolve('src/templates/Products.jsx')
 
   const result = await graphql(`
-      query GET_PRODUCTS {
-        allStripePrice {
-          edges {
-            node {
-              id
-              product  {
-                name
-                metadata {
-                  img
-                  description
-                  vegan
-                }
+    query GET_PRODUCTS {
+      allStripePrice {
+        edges {
+          node {
+            id
+            product  {
+              name
+              metadata {
+                img
+                description
+                vegan
               }
-              unit_amount
             }
+            unit_amount
           }
         }
       }
-    `)
+    }
+  `)
 
   if (result.errors) throw result.errors
 
   result.data.allStripePrice.edges.forEach(({ node }) => {
     createPage({
       path: `${node.id}`,
-      component: productTemplate
+      component: productTemplate,
+      context: node
     })
   })
 }
