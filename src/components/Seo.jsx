03222b7a6baf359a5939712a41415dae6c22450d
Jose Cuevas <josecueram@gmail.com>
2020-10-04 14:46:46 -0500

    [UPDATE] SEO defaultTitle asignment

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -26,7 +26,7 @@ function SEO ({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata.title
+  const defaultTitle = title || site.siteMetadata.title
 
   return (
     <Helmet
